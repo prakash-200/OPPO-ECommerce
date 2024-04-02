2dfb5b7a04c03ad729dd68dd655002c42cb9de82
prakash-200 <prakashrajrko1@gmail.com>
2024-04-02 08:47:12 +0530

    first commit

---
+++ backend/controllers/productController.js
@@ -0,0 +1,34 @@
+const ProductModel = require('../models/prouctModel');
+
+//Get products API - /api/v1/products
+exports.getProducts = async(req,res,next) => {
+    const query = req.query.keyword?{name : {
+        $regex: req.query.keyword,
+        $options: 'i'
+    }}:{}
+
+    const products = await ProductModel.find(query);
+    res.json({
+        success: true,
+        products
+    })
+}
+
+//Get single products API - /api/v1/products/:id
+exports.getSingleProduct = async (req,res,next) => {
+    try {
+        const product = await ProductModel.findById(req.params.id);
+        res.json({
+            success: true,
+            product
+        })
+        
+    } catch (error) {
+        res.status(404).json({
+            success: false,
+            messsage: error.message
+        })
+        
+    }
+
+}
